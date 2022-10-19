be5ade40b36847d92e3b454cd80a3499cbefca27
AleksandraIP <85641733+AleksandraIP@users.noreply.github.com>
2022-10-19 15:18:26 +0200

    about

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Info(props){
+    return(
+        <div>
+            <img width="300px" src={props.img} alt=""/>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Telefon: {props.tel}</p>
+            <p>E-mail: {props.email}</p>
+            <p>Nationality: {props.nationality}</p>
+        </div>
+    )
 }
 
-export default App;
+function Pet(props){
+    return(
+        <div>
+            <img width="400px" src={props.avatar} alt=""/>
+            <h2>Name: {props.name}</h2>
+            <p>Breed: {props.breed} </p>
+            <p>Age: {props.age}</p>
+        </div>
+    )
+}
+export default function App(){
+    return(
+        <div className="App">
+         <Info img={process.env.PUBLIC_URL + 'Oleksandra.jpg'} name="Oleksandra Popova" age="29" tel="079 111 11 11" email="aleksandraip@ukr.net" nationality="Ukrainian"></Info>
+            <Pet avatar={process.env.PUBLIC_URL + 'vasa.jpg'} name='Vasya' breed='British' age='10 year'></Pet>
+        </div>
+    );
+}
